462feb732a3c8da6935823915c6a72742a83283f
NIKITA <>
2023-03-25 19:03:06 +0300

    Присвоил новой переменной длинну слова в массиве

--- final_test_work.cs
+++ final_test_work.cs
@@ -3,14 +3,15 @@ Console.WriteLine("Please enter line: ");
 string Line = (Console.ReadLine());
 string[] words = Line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 int LengthString = words.Length;
-int i=0;
+int i = 0;
 string[] NewArray = new string[LengthString];
-for (i=0; i<LengthString; i++)
- {
-if (words[i].Length <=3)
+for (i = 0; i < LengthString; i++)
+{   
+    int wordlen = words[i].Length;
+    
+    if (wordlen <=3)
     {
-        NewArray[i]=words[i];
-        
-
+        NewArray[i] = words[i];
+        Console.Write(NewArray[i] + " ");
     }
- }
+}
