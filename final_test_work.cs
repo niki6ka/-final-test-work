2d8b5a845127e138379d22aa1de9116e3eecd1d3
NIKITA <>
2023-03-25 18:51:04 +0300

    проверяем длинну слова

---
+++ final_test_work.cs
@@ -0,0 +1,16 @@
+﻿// Ввести строку из которой выдернуть в отдельный массив элементы строки меньше или равных 3 символам
+Console.WriteLine("Please enter line: ");
+string Line = (Console.ReadLine());
+string[] words = Line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+int LengthString = words.Length;
+int i=0;
+string[] NewArray = new string[LengthString];
+for (i=0; i<LengthString; i++)
+ {
+if (words[i].Length <=3)
+    {
+        NewArray[i]=words[i];
+        
+
+    }
+ }
